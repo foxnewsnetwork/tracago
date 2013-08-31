b0be23301b803f94ae920dbdd8f519957025cd2b
Thomas Chen <foxnewsnetwork@gmail.com>
2013-08-30 18:57:13 -0700

    initial commit, rolled in spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracago::Application.config.secret_key_base = '2dd86241f5c168233c559cf6956d7548e912593756bd7a8cb227334183b706902d182a94bffa0477de0a0b437294a6262f112696bf121d0c7d752c865ab6b87e'
