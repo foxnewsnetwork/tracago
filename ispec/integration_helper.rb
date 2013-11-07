abbed42fe345565c3bd75932022d3dad5185356e
Thomas Chen <foxnewsnetwork@gmail.com>
2013-11-07 09:24:42 -0800

    moved the integrations out

--- ispec/integration_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'capybara'
-require 'ffaker'
-
-Capybara.default_driver = :selenium
-require Rails.root.join "spec", "integrations", "action"
-Dir[Rails.root.join("spec", "integrations", "*.rb")].each { |f| require f }
--- ispec/integrations/actions/visits.rb
+++
@@ -1,5 +0,0 @@
-class ChineseIntegration::Actions::Visits < ChineseIntegration::Action
-  def call(url)
-    state.visit url
-  end
-end
